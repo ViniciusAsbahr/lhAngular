6c846d293e0d49dab0c39456a19e70afbc528b05
ViniciusAsbahr <viniciusluisasbar15@gmail.com>
2022-03-05 14:44:00 -0300

    pripri

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { MuralVagasComponent } from './mural-vagas/mural-vagas.component';
+import { PainelVagasComponent } from './painel-vagas/painel-vagas.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MuralVagasComponent,
+    PainelVagasComponent,
+    MenuComponent,
+    RodapeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/mural-vagas/mural-vagas.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { VagasService } from '../vagas.service';
+import { vaga } from '../models/Vagas.model';
+
+@Component({
+  selector: 'app-mural-vagas',
+  templateUrl: './mural-vagas.component.html',
+  styleUrls: ['./mural-vagas.component.css']
+})
+export class MuralVagasComponent implements OnInit {
+
+  public vagas: Vaga[] = [];
+
+  constructor(private _vagasService: VagasService) { }
+
+  ngOnInit(): void {
+    this.listarVagas();
+  }
+
+  listarVagas(){
+  this._vagasService.getVagas()
+    .subscribe(
+      retornaVaga => {
+        this.vagas = retornaVaga.map(
+          item => {
+            return new vaga();
+          }
+        )
+      }
+    )
+  }
+
+}
